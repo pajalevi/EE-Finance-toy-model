845ac34a96f7623a858a3dd59495f7bf29e84bc5
Patricia L <pajalevi@gmail.com>
2014-04-04 17:13:39 -0700

    trying to modularize the model

---
+++ modelinit_modular.R
@@ -0,0 +1,149 @@
+#----------------------------------------------
+# modelinit_bank_hurdle.R
+# Output of this model is loan terms given by a 
+# bank to meet their hurdle rate given a
+# certain type of loan and incentive structure
+#
+# this script initializes the model inputs and
+# calls bank_hurdle.R
+#
+# This script is modified from model_initialize.R
+#
+# Patricia Levi 03/2014
+#----------------------------------------------
+
+
+#source model contents
+source('W:\\Research\\Energy Efficiency\\EE Finance toy model\\bank_hurdle.R')
+
+# source newtons method solver
+source('W:/Research/Energy Efficiency/EE Finance toy model/newton.solve.R')
+
+source('W:\\Research\\Energy Efficiency\\EE Finance toy model\\excel_finance_functions.R')
+### loads some pre-made R-copies of excel's finance functions. Thanks, the Internet! ###
+### included: NPV(rate, values), 
+#             IRR(x, start=0.1), 
+#             FV(rate, nper, pmt, pv = 0.0, type = 0), 
+#             PV(rate, nper, pmt, fv = 0.0, type = 0), 
+#             PMT(rate, nper, pv, fv=0, type=0)
+
+
+#-------------------#
+# Initialize Inputs #
+#-------------------#
+#----------------------------------------------
+# NB: any of the initial settings can be single numbers
+#     OR sets of testable numbers (eg. c(1,2,3) or seq(1,3,1))
+#----------------------------------------------
+  
+  #---------------#
+  # retrofit info #
+  #---------------#
+    eecost=13000  #cost of energy upgrade ($) - energy upgrade CA agv according to Zimring
+    savings.yr=2000 #yearly savings ($)
+    ee.lifetime = 15
+    
+  #----------------#
+  # financing info #
+  #----------------#
+    tenor = 10# seq(5,15,by=5)#15 #loan tenor
+    # not really in use currently
+#    loan.frac = 1# fraction of eecost covered by loan     
+
+  #---------------#
+  # discount info #
+  #---------------#
+    bank.hurdle = .0515#cost of capital http://pages.stern.nyu.edu/~adamodar/New_Home_Page/datafile/wacc.htm #.0075 
+    user.discount = .15 #seq(.05,.25,by=.02)
+    gvt.discount = .04#seq(.01,.05,by=.01) 
+
+  #------------#
+  # risky info #
+  #------------#
+    risk.adjust =.4 # as in (Amato, J., 2005) this number is used as follows
+#!#                 # additional return (basis pts) required to compensate for risk = expected loss(i.e. 1-ev.pmt) * risk.adjust
+                    # same source gives the .4 number
+    chance.full.loss = .13 # i.e. chance of defaulting by end of loan tenor. 13% ~= 4% expected loss
+    recovery = .4 #pct of loan that is recovered on default
+    
+  #-------------------#
+  # intervention info #
+  #-------------------#
+    # different types of interventions are separated by a "\n # \n"
+    #
+    interest.buydown = c(0,seq(0.01,0.15,by=0.01)) #amount that the gvt will buydown the interest rate
+    #
+    upfront.rebate = 0#eecost * c(0,.05)#seq(0,0.2,by=0.05)# .20 #20 percent buydown
+    #
+    #TO TURN OFF LOAN LOSS RESERVE, SET LSR=0
+    LPCR = c(.02,.03,.04,.05,.06,.07) # loan pool coverage ratio. usually around 5-10%
+    # cisco devries; "$10m gives about $200m of financing" for PACE.
+    LSR = c(0,.8) #loss-share ratio, usually ~90%. 0% --> no LLR
+
+#--------------------#
+# prepare inputs     #
+# for multiple values#
+#--------------------#
+
+# use expand.grid, which accepts a list of vectors
+inlist = list(eecost = eecost, 
+              savings.yr=savings.yr, 
+              ee.lifetime = ee.lifetime, 
+              tenor=tenor, 
+#              loan.frac=loan.frac, 
+              bank.hurdle = bank.hurdle, 
+              user.discount = user.discount, 
+              gvt.discount = gvt.discount, 
+              chance.full.loss = chance.full.loss,
+              recovery=recovery,
+              LPCR = LPCR,
+              LSR = LSR,
+              interest.buydown = interest.buydown, 
+              upfront.rebate = upfront.rebate,
+              risk.adjust=risk.adjust)
+
+inputs = expand.grid (inlist)
+
+#--------------------#
+### call the model ###
+#--------------------#
+
+results = bankmodel(inputs)
+
+# create some indices for viewing different subsets of the results
+LLR = results[,"LSR"]>0 & results[,"upfront.rebate"]==0 & results[,"interest.buydown"]==0
+IRB = results[,"LSR"]==0 & results[,"upfront.rebate"]==0 & results[,"interest.buydown"]>0
+rebate = results[,"LSR"]==0 & results[,"interest.buydown"]==0
+nothing = results[,"LSR"]==0 & results[,"upfront.rebate"]==0 & results[,"interest.buydown"]==0
+no.loss=results[,"chance.full.loss"]==0
+no.recovery = results[,"recovery"]==0
+
+
+#-------------------------------------#
+### to be implemented in the future ###
+#-------------------------------------#
+# partition bit of script into functions
+#guarantee.frac = #fraction of loan that is guaranteed
+# interest rate buydown
+# upfront rebate
+#     showing the cost comparison between the above two
+# loan-loss reserve
+# incorporate risk/cost of customer default
+
+# set-up to create a dataset for x vs y comparison
+# ex - how cost to government changes as interest rate buydown changes
+#     -** plot cost to gvt on x axis, NPV to user on Y, compare interest rate and upfront rebate
+#    - how user NPV changes as user discount changes
+#    - how costs change as function of loan tenor
+#    - how risk of default changes banks' costs (i.e. importance of knowing if ee is notably less risky)
+# output kWh (or $) saved per $ spent by gvt
+
+#change model output to output a dataset, not print it
+
+#!# Figure out how hurdle rate is some combo of riskiness and discount rate...
+#!# so it would sort of account for part of the change_full_loss stuff??
+
+# Tenor may be a model output?
+
+# incorporate uncertainty in energy savings
+
---
+++ modular_model.R
@@ -0,0 +1,275 @@
+#----------------------------------------------
+# bank_hurdle.R
+# Output of this model is loan terms given by a 
+# bank to meet their hurdle rate given a
+# certain type of loan and incentive structure
+#
+# this script is initialized by
+# modelinit_bank_hurdle.R
+#
+# This script is modified from model1.R
+#
+# Patricia Levi 03/2014
+#----------------------------------------------
+
+bankmodel = function (input) {
+  output = matrix(nrow=nrow(input), ncol = 13, dimnames=list(c(), c("expected.loss.pct","expected.loss","interest.user","interest.bank","loan.payment.user",
+                                                                   "loan.payment.bank","simple.payback.yrs","user.NPV","bank.NPV","gvt.cost.NPV",
+                                                                   "gvt.reserve.size","gvt.llr.oppcost","buydown.cost")))
+  
+  #----------------------------------------------#
+  # Iterate through all the rows of input matrix #
+  #----------------------------------------------#
+  for(i in 1:nrow(input)){
+    ## check for reasonable values ##
+    if (input$tenor[i] > input$ee.lifetime[i]) print("WARNING: loan tenor is longer than the lifetime of the EE project!")
+    
+    
+    # Modify loan terms according to gvt intervention
+    loan.amt = input$eecost[i] - input$upfront.rebate[i]
+    
+    #----------------------------------#
+    # assume monthly loan payment &    #
+    # calculate monthly rates          #
+    #----------------------------------#
+    bank.hurdle.mo = mo.rate(input$bank.hurdle[i])
+    user.discount.mo = mo.rate(input$user.discount[i])
+    gvt.discount.mo = mo.rate(input$gvt.discount[i]) # used for LLR opportunity cost
+    npmt = input$tenor[i]*12 # number of payments over tenor of loan    
+    
+    #--------------------------#
+    # Back out Interest Rate   #
+    # to bank and then to user #
+    # given bank NPV = 0       #
+    #--------------------------#
+      
+      #-----------------#
+      # find P(default) and then the probability that the bank gets 
+      # paid by either the bank or the LLR (= ev.pmt)
+      # since this is expected value, I can treat LSR (the fraction the bank must pay) as a proability
+      #-----------------#
+      loss.stats = losses(npmt, input$chance.full.loss[i],input$recovery[i],input$LSR[i])
+            
+
+      loan.payment = pmt.from.risk()
+  
+
+      #----------------------------------#
+      # solve for interest rate given to bank
+      # given loan.payment & PV=loan.amt
+      # loan.payment = (interest.rate * loan.amt)/(1-(1+interest.rate)^-n)
+      # use newton.solve.R   
+      #----------------------------------#
+      fx = expression(((x * loan.amt)/(1-(1+x)^-npmt))-loan.payment)
+      interest.rate.mo = tail(newton.solve(f=fx,loan.amt=loan.amt, loan.payment=loan.payment,npmt=npmt),n=1)
+      interest.rate = yr.rate(interest.rate.mo)
+      
+      
+    #------------------------------#
+    # Calculate user interest rate, 
+    # loan payment, (would differ w/ interest rate buydown)
+    # and net present value #
+    # account for interest-rate buydown, upfront rebate
+    #------------------------------#
+    
+    # disallow interest rate buydowns that would result in a negative interest rate for the user
+    if(interest.rate - input$interest.buydown[i] >= 0){
+      interest.user = interest.rate - input$interest.buydown[i]
+    } else {
+      interest.user=0
+      # uncomment if you'd like to display the ACTUAL buydown, 
+      # not the buydown that was initially set
+      input$interest.buydown[i] = interest.rate
+    }
+
+    interest.user.mo = mo.rate(interest.user)
+    loan.payment.user = - pmt(interest.user.mo,nper=npmt,pv=loan.amt)
+    # pmt returns NaN if interest = 0; catch that & replace
+    if(is.nan(loan.payment.user)){ 
+      loan.payment.user=loan.amt/npmt
+    }
+      
+    loan.NPV.user = loan.amt - loan.payment.user/user.discount.mo * (1 - (1/((1+user.discount.mo)^(npmt)))) 
+    #print(paste("Loan NPV to user: $", loan.NPV.user,". Montly payments: $", loan.payment))
+    
+    savings.NPV.user = input$savings.yr[i]/input$user.discount[i] * (1 - (1/((1+input$user.discount[i])^(input$tenor[i])))) 
+    #print(paste("savings NPV to user:",savings.NPV.user))
+    
+    user.NPV = savings.NPV.user + loan.NPV.user + input$upfront.rebate[i] - eecost
+
+    #calculate years for a simple payback period
+    simple.payback = loan.amt/input$savings.yr[i]
+    
+    #------------------------------#
+    # Calculate cost to government #
+    #------------------------------#
+
+      #-----------------#
+      # find loan.loss reserve size
+      #-----------------#
+      reserve.size = loan.amt * input$LSR[i] * input$LPCR[i]
+      
+      #-------------------
+      # INTEREST RATE BUYDOWN COST
+      # appropriate discount rate is the bank's
+      # b/c it is typically done as an upfront lump sum.
+      #-------------------
+      gvt.contribution = loan.payment-loan.payment.user
+      buydown.NPV.gvt = -1* gvt.contribution/bank.hurdle.mo * (1 - (1/((1+bank.hurdle.mo)^(npmt))))
+
+      #-------------------
+      # LOAN LOSS RESERVE COST
+      # incorporate opportunity cost of LLR into gvt NPV
+      # assume that alternative would be to invest the LLR
+      # in a set of projects that pay interest at rate r, monthly
+#      pmts = reserve.size*gvt.discount.mo
+#      llr.opp.cost = pmts * (1-(1+gvt.discount.mo)^-npmt)*gvt.discount.mo #discounted stream of interest on the llr
+      
+    
+    gvt.cost.NPV = buydown.NPV.gvt - input$upfront.rebate[i] - reserve.size #- llr.opp.cost 
+     
+    
+    #---------------------------------#
+    # SANITY CHECK                    #
+    # Calculate bank NPV, should = 0  #
+    #---------------------------------#
+    #loan.NPV.bank = loan.payment*EV.NPV.factor  # guaranteed to result in 0 bank NPV
+    
+    loan.NPV.bank = loan.payment.user*EV.NPV.factor - buydown.NPV.gvt # accounts for zero risk of IRB payment
+
+    bank.NPV = loan.NPV.bank - loan.amt
+        
+    
+    #------------------------------#
+    # Fill out  output matrix      #
+    #------------------------------#    
+    d=4
+    output[i,"expected.loss.pct"]=expected.loss
+    output[i,"expected.loss"]=.01*expected.loss*loan.amt
+    output[i,"user.NPV"]=round(user.NPV,digits=d)
+    output[i,"bank.NPV"]=round(bank.NPV,digits=d)
+    output[i,"gvt.cost.NPV"]=round(gvt.cost.NPV,digits=d)
+    output[i,"gvt.reserve.size"]=round(reserve.size,digits=d)
+ #   output[i,"gvt.llr.oppcost"]=llr.opp.cost
+    output[i,"interest.user"]=round(interest.user*100,digits=d)
+    output[i,"interest.bank"]=round(interest.rate*100,digits=d)
+    output[i,"loan.payment.user"]=round(loan.payment.user,digits=d)
+    output[i,"loan.payment.bank"]=round(loan.payment,digits=d)
+    output[i,"simple.payback.yrs"]=simple.payback
+    output[i,"buydown.cost"]=round(buydown.NPV.gvt,digits=d)
+
+  }
+
+#-----------------#
+# Prepare output  #
+#-----------------#
+  output = cbind(input,output)
+  # cost to government
+  
+  # chance of user default 
+  
+  # expected return for bank
+  
+  #cat(paste("Net present value to user is \n $", round(user.NPV,2),"\n"))
+  #cat(paste("Net present value to bank is \n $", round(bank.NPV,2),"\n"))
+  #cat(paste("Net present value of cost to gvt is \n $", round(gvt.cost.NPV,2),"\n"))
+  return(output)
+  
+  # print out initialization info
+  # provide year 1 payment & bill savings. (?)
+}
+
+#--------------------------------#
+# Helper functions for the model #
+#--------------------------------#
+
+  #--------------------------------#
+  # calculate loan payment given
+  # risk information and loan amount
+  #--------------------------------#
+  pmt.from.risk = function(input, i, no.default.chance, loan.amt,
+                          expected.loss.noLLR= NULL){
+    # loan pool coverage ratio (LPCR) - if expected.loss > LPCR --> problems!
+    # Attempt to factor this in the the small.LLR adjustment
+    #!# I HAVE NO IDEA IF THIS IS CORRECT IN ANY WAY
+    small.LLR = 1
+    if (input$LPCR[i] < expected.loss.noLLR){ 
+      warning("LPCR is less than expected loss! The pool will be exhausted!") 
+      small.LLR = input$LPCR[i] / expected.loss.noLLR
+    }
+    
+    #expected value of payment = P(no defaults) + P(LLR OR (Loan Security/Recovery) pays bank AND the user defaults)
+    ev.pmt = no.default.chance + ((1-no.default.chance) * 
+                                  (1-(1-input$recovery[i]) *(1-(input$LSR[i]* small.LLR))))
+    # ASSUMPTION: if recovery is 40%, and LSR is 80%, gvt makes bank whole for 80% of outstanding balance [leaving the bank with 12% loss]
+    # Credit Enhancement Overview Guide wording: 
+    # LSR = "the percentage of a lender's loss on each customer default that they can recover." 
+    
+    #-------------------#
+    # find Loan Payment to bank #
+    #-------------------#
+    ## calculate conversion factor from PV to loan payment ##
+    ## from k=1 to =n, Sum of (1-(1-p(d))^k)(1+bank hurdle rate)^-k
+    #!# Add, to hurdle rate in this case, (1-ev.pmt)* risk.premium.factor
+    #!# but you wouldn't use that addition to discount the IRB payments, for example.
+    #!# need to think carefully about when to use the risk-free and risky rate
+    bank.risk.premium = input$risk.adjust[i]*(1-ev.pmt)
+    
+    discount.stream = (1+mo.rate(bank.hurdle + bank.risk.premium))^-k
+    EV.NPV.factor = sum(ev.pmt * discount.stream) # i.e. expected value & NPV conversion
+    
+    loan.payment = loan.amt / EV.NPV.factor
+    
+    return(loan.payment)
+  }
+
+  #--------------------------------#
+  # calculate of no default over tenor
+  # and value of expected loss
+  # return as a paired list
+  #--------------------------------#
+  losses = function (npmt, chance.full.loss, recovery, LSR){
+    output = list()
+    
+    k = seq(1,npmt,by=1) #list of numbers from 1 to npmt
+    
+    # chance of default in a given month
+    # assume a uniform chance of default over the whole loan tenor
+    output$default.chance.mo = sub.prob(chance.full.loss,npmt) #ie chance of default in a given payment cycle (month)
+    # cumulative chance of no default by a given month
+    output$no.default.chance = (1-default.chance.mo) ^ k #this makes a list with length 12*tenor=npmt
+    
+    # for bookkeeping, what % of total value is expected to be lost?    
+    output$expected.loss = sum((1-no.default.chance)*((1-recovery) *(1-LSR)))/(npmt)
+    output$expected.loss.noLLR = sum((1-no.default.chance)*(1-recovery))/(npmt)
+    
+    return(output)
+   }
+
+  #--------------------------------#
+  # calculate probability of an event in a sub-period, 
+  # given overall rate & number of sub-periods
+  #--------------------------------#
+  sub.prob = function (prob, n){
+    if(prob>1) {stop("prob is greater than 1, which is breaking the math")}
+    r_p =1-((1-prob)^(1/n))
+    return(r_p)    
+  }
+
+  #--------------------------------#
+  # deal with compounding interest #
+  #--------------------------------#
+  # calculate the monthly rate from yearly 
+   mo.rate = function (yr.rate,n=12){
+    #if(yr.rate>1) {stop("yearly rate is greater than 100%, which is breaking the math")}
+    monthly = ((1+yr.rate)^(1/n))-1
+    return(monthly)
+  }
+  
+  #calculate compounded yearly rate from nominal monthly rate
+  yr.rate = function (mo.rate, n=12){
+    if(mo.rate>1) {stop("monthly rate is greater than 100%, which is breaking the math")}
+    yearly =((1+mo.rate)^n)-1
+    return(yearly)    
+  }
+  
